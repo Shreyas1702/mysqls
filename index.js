f05d2820563fde3c12a3c317e040d25afc9e445d
Shreyas Keni <sskeni1702@gmail.com>
2023-07-01 15:27:52 +0530

    Lastly

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const engine = require("ejs-mate");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { connect } = require("http2");
-var app = express();
 const session = require("express-session");
+var app = express();
 app.use(
   session({
+    proxy: true,
+    key: "keyin",
     secret: "webslesson",
-    resave: true,
+    resave: false,
     saveUninitialized: true,
   })
 );
+app.set("trust proxy", 1);
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -23,8 +26,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  console.log(req.session.user_id);
-  res.render("homepage.ejs", req.session);
+  const data = req.session.user_id;
+  res.render("homepage.ejs", { data });
+});
+
+app.get("/customer", (req, res) => {
+  const data = req.session.user_id;
+  res.render("customerpage.ejs", { data });
 });
 
 app.post("/logins", (req, res, next) => {
@@ -32,26 +40,26 @@ app.post("/logins", (req, res, next) => {
   const cust2 = "customer2";
   var weight1,
     weight2,
-    totalweight,
+    tweight,
     bcount1,
     bcount2,
-    totalbcount,
+    tbcount,
     quantity1,
     quantity2,
     tquantity;
   connection.query(
-    "select * from user where id=?",
+    "select * from user where username=?",
     [req.body.id],
     (err, row) => {
       if (err) {
         console.log(err);
       } else {
         req.session.user_id = req.body.id;
-        console.log("HII");
+        console.log(row);
         if (row[0].password === req.body.password) {
           console.log(row);
 
-          if (row[0].type == "admin") {
+          if (row[0].username == "admin") {
             console.log("Neither");
             connection.query(
               "select sum(quantity) as tquantity from customer where onwer = ?",
@@ -119,8 +127,76 @@ app.post("/logins", (req, res, next) => {
   );
 });
 
-app.get("/admins", (req, res) => {
-  res.render("adminpage.ejs");
+app.get("/admin", (req, res) => {
+  const cust1 = "customer1";
+  const cust2 = "customer2";
+  var weight1,
+    weight2,
+    tweight,
+    bcount1,
+    bcount2,
+    tbcount,
+    quantity1,
+    quantity2,
+    tquantity;
+
+  connection.query(
+    "select sum(quantity) as tquantity from customer where onwer = ?",
+    [cust1],
+    (err, row) => {
+      quantity1 = row[0].tquantity;
+    }
+  );
+  connection.query(
+    "select sum(quantity) as tquantity from customer where onwer = ?",
+    [cust2],
+    (err, row) => {
+      quantity2 = row[0].tquantity;
+    }
+  );
+  connection.query(
+    "select sum(weight) as tweight from customer where onwer = ?",
+    [cust1],
+    (err, row) => {
+      weight1 = row[0].tweight;
+    }
+  );
+  connection.query(
+    "select sum(bcount) as tbcount from customer where onwer = ?",
+    [cust1],
+    (err, row) => {
+      bcount1 = row[0].tbcount;
+    }
+  );
+  connection.query(
+    "select sum(weight) as tweight from customer where onwer = ?",
+    [cust2],
+    (err, row) => {
+      weight2 = row[0].tweight;
+    }
+  );
+  connection.query(
+    "select sum(bcount) as tbcount from customer where onwer = ?",
+    [cust2],
+    (err, row) => {
+      console.log(typeof weight1);
+      bcount2 = row[0].tbcount;
+      tbcount = Number(bcount1) + Number(bcount2);
+      tweight = weight1 + weight2;
+      tquantity = Number(quantity1) + Number(quantity2);
+      res.render("adminpage.ejs", {
+        weight1,
+        weight2,
+        tweight,
+        bcount1,
+        bcount2,
+        tbcount,
+        quantity1,
+        quantity2,
+        tquantity,
+      });
+    }
+  );
 });
 
 app.post("/customers", (req, res) => {
@@ -140,7 +216,7 @@ app.post("/customers", (req, res) => {
     cust.cquantity,
   ];
   connection.query(
-    "insert into customer values(?)",
+    "insert into customer(odate,cname,onwer,item,quantity,weight,rshipment,trackid,ssize,bcount,specification,cquantity) values(?)",
     [custData],
     (err, rows) => {
       if (err) {
@@ -152,9 +228,9 @@ app.post("/customers", (req, res) => {
   );
 });
 
-app.get("/customer", (req, res) => {
-  console.log(req.session);
-  res.render("customerpage.ejs");
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 app.get("/login", (req, res) => {
